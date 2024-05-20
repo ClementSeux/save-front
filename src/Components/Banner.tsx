3fcc130411ca62bcaee24e01bd19ad6c3b566574
Clement Seux <clement.seux@gmail.com>
2024-05-20 17:05:14 +0200

    refactor: Update image source path in Banner component

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,7 +1,7 @@
 const Banner = () => {
   return (
     <div id="banner">
-        <img src="/images/banner.png" alt="banner" />
+        <img src="./images/banner.png" alt="banner" />
         <button id="banner-button">
           <span>
             Découvrir les offres &#8594;
