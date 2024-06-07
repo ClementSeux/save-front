3f133871e69a9843f78761f66d07a9cd00a093f6
Clément SEUX <clement.seux@axxes.fr>
2024-06-07 02:25:11 +0200

    refactor: Update imageTable file to use uppercase file extensions for item images

--- src/utils/imageTable.ts
+++ src/utils/imageTable.ts
@@ -1,13 +1,13 @@
 const imageTable = {
-    1: "dentifrice.png",
-    2: "yaourts.png",
-    3: "gel_douche.png",
-    4: "mascara.png",
-    5: "rouge.png",
-    6: "shampoing.png",
-    7: "lays.png",
-    8: "liquide_vaisselle.png",
-    9: "brioche.png",
+    1: "dentifrice.PNG",
+    2: "yaourts.PNG",
+    3: "gel_douche.PNG",
+    4: "mascara.PNG",
+    5: "rouge.PNG",
+    6: "shampoing.PNG",
+    7: "lays.PNG",
+    8: "liquide_vaisselle.PNG",
+    9: "brioche.PNG",
 };
 
 export default imageTable;
